68031ad36b46ad3076b44fadcc11dee4533c116a
pitoneverde <pitoneverde181@gmail.com>
2025-05-05 18:08:19 +0200

    merge conflict

--- src/main.c
+++ src/main.c
@@ -6,24 +6,14 @@
 /*   By: sabruma <sabruma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:51:36 by plichota          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/05/05 17:57:56 by sabruma          ###   ########.fr       */
-=======
-/*   Updated: 2025/05/05 17:53:40 by plichota         ###   ########.fr       */
->>>>>>> 072d74a48ef1755bb9b07fd6835ef471c63b699d
+/*   Updated: 2025/05/05 18:06:47 by sabruma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD
 int main (int argc, const char *argv[], const char *envp[])
 {
-
+	
 	return (0);
-=======
-int main(void)
-{
-    return 0;
->>>>>>> 072d74a48ef1755bb9b07fd6835ef471c63b699d
 }
