9d9b9764666862508f1d883a647536264c45a722
pitoneverde <pitoneverde181@gmail.com>
2025-05-26 15:39:45 +0200

    added tested leaf nodes

--- include/ast.h
+++ include/ast.h
@@ -66,11 +66,12 @@ char *ast_to_string(const t_ast *node);
 // to_string
 void ast_stringify(t_ast *node);
 
-// types (to move into parser)
+// types of nodes
 int ast_is_operator(const t_ast *node);
 int ast_is_command(const t_ast *node);
 int ast_is_simple_pipeline(const t_ast *node);
 int ast_is_redirection_chain(const t_ast *node);
+int ast_is_leaf(const t_ast *node);
 
 // errors and validation (also to move into parser)
 int ast_has_error(const t_ast *node);
@@ -90,12 +91,14 @@ void ast_traverse_pre_ctx(t_ast *node, void (*visit)(t_ast *, void *), void *dat
 void ast_traverse_post(t_ast *node, void (*visit)(t_ast *));
 void ast_traverse_post_ctx(t_ast *node, void (*visit)(t_ast *, void *), void *data);
 
-// filtering and flattening
+// filtering
 t_ast **ast_filter(t_ast *root, int (*predicate)(const t_ast *));
 t_ast **ast_filter_n(t_ast *root, int (*predicate)(const t_ast *), size_t *dim);
+t_ast **ast_leaf_nodes(t_ast *root);
+t_ast **ast_leaf_nodes_n(t_ast *root, size_t *dim);
+
 
 // TODO:
-// t_ast **ast_leaf_nodes(t_ast *root, size_t *out_count);
 // t_ast **ast_flatten_pre(t_ast *root, size_t *out_count);
 // t_ast **ast_flatten_post(t_ast *root, size_t *out_count);
 // void ast_traverse_level(t_ast *node, void (*visit)(t_ast *));
--- src/ast/ast_filter.c
+++ src/ast/ast_filter.c
@@ -1,6 +1,5 @@
 #include "ast.h"
 
-
 // wraps the t_ast nodes into t_list to filter
 static void ast_filter_visit(t_ast *node, void *data)
 {
@@ -52,3 +51,54 @@ t_ast **ast_filter_n(t_ast *root, int (*predicate)(const t_ast *), size_t *dim)
 		return (free(result), NULL);
 	return (result);
 }
+
+// Equivalent below, without filter utility
+int is_leaf_node(const t_ast *node)
+{
+	return (!node->left && !node->right);
+}
+
+t_ast **ast_leaf_nodes(t_ast *root)
+{
+	return ast_filter(root, ast_is_leaf);
+}
+
+t_ast **ast_leaf_nodes_n(t_ast *root, size_t *dim)
+{
+	return ast_filter_n(root, ast_is_leaf, dim);
+}
+
+// // Callback for extracting a leaf node
+// static void	collect_leaf_node(t_ast *node, void *ctx)
+// {
+// 	t_list **leaves = (t_list **)ctx;
+// 	if (!node->left && !node->right)
+// 		ft_lstadd_back(leaves, ft_lstnew(node));
+// }
+
+// // Collects all nodes without children in a NULL-terminated array
+// // leaves the tree untouched (shared pointers with arr)
+// // free only the array master ptr and then the original tree
+// t_ast **ast_leaf_nodes(t_ast *root)
+// {
+// 	t_ast	**result;
+// 	t_list	*leaves;
+
+// 	leaves = NULL;
+// 	ast_traverse_pre_ctx(root, collect_leaf_node, &leaves);
+// 	result = ast_lst_to_array(leaves);
+// 	ft_lstclear(&leaves, NULL);
+// 	return (result);
+// }
+
+// t_ast **ast_leaf_nodes_n(t_ast *root, size_t *dim)
+// {
+// 	t_ast **result;
+// 	t_list *leaves;
+
+// 	leaves = NULL;
+// 	ast_traverse_pre_ctx(root, collect_leaf_node, &leaves);
+// 	result = ast_lst_to_array_n(leaves, dim);
+// 	ft_lstclear(&leaves, NULL);
+// 	return (result);
+// }
